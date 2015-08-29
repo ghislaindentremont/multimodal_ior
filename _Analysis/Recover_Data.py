9dc387a725be65e255deacf16b0a33ccfb74e9e2
ghislaindentremont <ghislaindentremont@gmail.com>
2015-08-29 20:34:17 -0300

    Progress on Recover_Data file
    I haven't yet been testing this with actual files to see if every step
    is working along the way.

--- _Analysis/Recover_Data.py
+++ _Analysis/Recover_Data.py
@@ -1,5 +1,125 @@
 # IS there synchrony between vmrk files and asc files? NO
 
+""" I want:
+-id
+-trial_num
+-cue_location
+-cue_modality
+-target_location
+-target_modality 
+-target_response_rt
+	-target_on
+	-* get trialDone from asc files
+"""
+
+import fnmatch
+import os
+
+def integer_to_binary(number_string): 
+	number_interger = int(number_string)
+	number_binary = bin(number_interger)[2:] 
+	if len(number_binary) < 8:
+		number_binary = number_binary.zfill(8)
+
+	return(number_binary)	
+
+def get_relevant_data(filename):
+	    newFile = open(filename + "_new", "w") # open file in which the refined information from the original .vmrk will be put in 
+		newFile.write("trial_num	cue_location	cue_modality	target_location	target_modality	target_type	target_on_time\ttrial_start_time")
+
+		oldFile = open(filename, 'r')
+    	
+    	done = False
+		dataStarted = False
+
+		while not done:
+			newFile.write("\n")
+
+			line = oldFile.readline()
+			if not dataStarted:
+				if line[0:2] == "Mk":
+					dataStarted = True
+			if dataStarted:
+				if line == '':
+					done = True
+				elif line[0:2] == "Mk":
+					# first I want to get the trial variable 
+					i = 5
+					trial_num = line[2:i]
+					while not type(trial) == int:
+						i -= 1
+						trial_num = line[2:i] 
+					newFile.write(trial_num + "\t")
+					
+					# second I want to get binary derived variables
+					number_string = line.replace(' ','').split(',')[1].strip().strip('S')
+					if number_string == '':
+						pass
+					else:
+						integer_to_binary(number_string)
+
+						line = line.split(',')
+						line[1] = number_binary[::-1]
+
+						# and now extract parameters from binary and put into newFile 
+						for_label = line[1]		
+					
+						# start of trial or target on
+						if for_label[6] == "0":
+							
+							# cue location
+							if for_label[1] == "0":
+								newFile.write("left" + "\t")
+							else:
+								newFile.write("right" + "\t")
+							
+							# cue modality
+							if for_label[2] == "0":
+								newFile.write("visual" + "\t")
+							else:
+								newFile.write("auditory" + "\t")
+							
+							# target location
+							if for_label[3] == "0":
+								newFile.write("left" + "\t")
+							else:
+								newFile.write("right" + "\t")
+							
+							# target modality 
+							if for_label[4] == "0":
+								newFile.write("visual" + "\t")
+							else:
+								newFile.write("auditory" + "\t")
+							
+							# target type	
+							if for_label[5] == "0":
+								newFile.write("catch" + "\t")
+							else:
+								newFile.write("target" + "\t")
+
+							# NOW GET WHEN TARGET IS ON
+							
+						else:
+							labels.append("trial start")
+
+							# NOW GET WHEN TRIAL HAS STARTED 
+
+
+
+def walk_it_out():
+	# execute file in _Analysis, then go back one directory 
+	os.chdir('..')
+
+	# look at all .vmrk files in the walk through _Data 
+	for root, dirnames, filenames in os.walk('_EEG'):
+	    for filename in fnmatch.filter(filenames, '*.vmrk'): # It will be in multimodal_ior directory
+	    	get_relevant_data(filename)
+					
+ 
+    
+
+################## LOOKING AT PREVIOUS CODE BELOW 
+
 import os 
 os.chdir("C:\Users\ghislaindentremont\Documents\R\MultiIOR\VMRK")
 file = "multimodal_ior_forAlex.vmrk"
