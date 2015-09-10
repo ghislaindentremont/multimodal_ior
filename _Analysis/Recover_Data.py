0a54be5084838a2743589e323bee9ef40d955883
ghislaindentremont <ghislaindentremont@gmail.com>
2015-09-10 15:41:29 -0300

    Can Take Both Files and Make Big Lists

--- _Analysis/Recover_Data.py
+++ _Analysis/Recover_Data.py
@@ -103,7 +103,7 @@ def process_vmrk(filedir, filename):
 	oldFile.close()
 								 
 # THIS WORKS 
-# this walks through given directory to find files with given extention to apply to them the given function. NOTE: the filename (output) is of type 'str'
+# this walks through given directory to find files with given extention to append them to a list. 
 def walk_it_out(directory, extention):
 	filepaths = []
 	for root, dirnames, filenames in os.walk(directory):
@@ -179,6 +179,7 @@ def process_asc(filedir, filename):
 	saccadesFile.write('\n'.join(['\t'.join(thisSaccade) for thisSaccade in saccades]))
 	saccadesFile.close()
 
+# THIS WORKS 
 # function to process saccades, blinks, samples, etc. files that were derived from asc files 
 def process_EM_files(filedir, filename):
 	os.chdir(filedir)
@@ -261,14 +262,125 @@ def deconstruct_filepaths(filepaths):
 		filedir_filename_tuples.append( (filedir, filename) )
 	return(filedir_filename_tuples)
 
-# THIS WORKS 
-def run_function(directory, extention, function):
+# THIS WORKS
+def walk_and_deconstruct(directory, extention):
 	filepaths = walk_it_out(directory, extention)
 	filedir_filename_tuples = deconstruct_filepaths(filepaths)
+	return filedir_filename_tuples
+
+# THIS WORKS 
+def run_function(directory, extention, function):
+	filendir_filename_tuples = walk_and_deconstruct(directory, extention)
 	path_orig = os.getcwd()
 	for (filedir, filename) in filedir_filename_tuples:
 		function(filedir, filename)
 		os.chdir(path_orig)
+
+# THIS WORKS 
+def find_missing_files(filedir, filename):
+	list = ["e03", "e04", "e05", "e06", "e07", "e08", "e09", "e10"]
+	participant_tuple= []
+	for participant in list:
+		if fnmatch.fnmatch(filename, "*"+participant+"*"):  
+			participant_tuple.append( (filedir, filename) )
+			break # might have misused break 
+	return participant_tuple
+
+# THIS WORKS 
+def get_relevant_tuples(directory, extention):
+	filedir_filename_tuples = walk_and_deconstruct(directory, extention)
+	relevant_tuples = []
+	for (filedir, filename) in filedir_filename_tuples:
+		relevant_tuple = find_missing_files(filedir, filename)
+		if not relevant_tuple:
+			pass
+		else:
+			relevant_tuples.append(relevant_tuple[0])
+	return relevant_tuples
+
+# THIS WORKS 
+# make list for each column 
+def get_column_lists(filedir, filename):
+	os.chdir(filedir)
+	newFile = open(filename, "r")
+	first_line = newFile.readline()
+	first_line = first_line.split("\t")
+	length_line = len(first_line)
+	big_list = [] # list within lists 
+	for i in range(length_line): # this should create a list with a list within it with just one item (header) for each column.
+		temp_list = []
+		temp_list.append(first_line[i])
+		big_list.append(temp_list)
+	lines = newFile.readlines()
+	for line in lines:
+		line = line.strip("\n")
+		line = line.split("\t")
+		for i in range(length_line):
+			big_list[i].append(line[i])
+	return big_list
+
+# THIS WORKS
+def rid_practice(big_list):
+	for i in range(len(big_list) ):
+		a_list = []
+		header = big_list[i][0]
+		body = big_list[i][41:]
+		a_list.append(header)
+		for item in body:
+			a_list.append(item)
+		big_list[i] = a_list 
+
+def ignore_NA(list, change): 
+	for item in list:
+		if item == "NA":
+			pass
+		else:
+			item2 = item - change
+			list[list.index(item)] = item2 
+
+# want to match .vmrk with _new EM file - assuming two big lists  
+def match_vmrk_EM_files(vmrk_big_list, EM_big_list):
+	length_list_EM = len(EM_big_list)
+	length_list_vmrk = len(vmrk_big_list)
+	if EM_big_list[1][1] == "practice":
+		rid_practice(EM_big_list)
+	else:
+		pass
+	EM = len(EM_big_list[0])
+	vmrk = len(vmrk_big_list[0])
+	if vmrk - EM == 40: # assume, same number prior to removing practice trials for EM, EM should now have 40 less 
+		for i in range(length_list_vmrk):
+			rid_practice(vmrk_big_list)
+		EM = len(EM_big_list[0])
+		vmrk = len(vmrk_big_list[0])
+		if vmrk - EM != 0:
+			return "ERROR: getting rid of practice trials did not solve the problem"
+		else:
+			pass 
+	elif vmrk - EM == 0: # case where vmrk already was missing practice trials 
+		pass 
+	elif vmrk - EM < 40: # I should check before hand for the cases where the EEG may have timed out - I don't think there are any of the lost participants 
+		return "ERROR: vmrk file is missing some files at the end of the experiment."
+	else:
+		return "ERROR: vmrk should not be more than practice trials of EM at this point, but is..."
+	EM_time_start = EM_big_list[3][1]
+	vmrk_time_start = vmrk_big_list[6][1]
+	time_diff = EM_time_start - vmrk_time_start  # assuming that eeg was always started after Eyetracker
+	if time_diff < 0:
+		return "vmrk time start is greater than EM time start - should always be opposite given experimental design"
+	else:
+		pass
+	EM_big_list[3][1:] = EM_big_list[3][1:] - time_diff # change trial start time 
+	EM_big_list[4][1:] = EM_big_list[4][1:] - time_diff # change trial end time 
+	ignore_NA(EM_big_list[7][1:], time_diff) # change latest saccade time
+	ignore_NA(EM_big_list[8][1:], time_diff) # change latest blink time 
+	return EM_big_list[3][1:] - vmrk_big_list[6][1:] # now check to be sure that trial start times match
+
+
+# get relevant tuples then get biglist for each, then match?
+
+
+
 
 # convert (does not replace old file) all edfs [ONLY NEED TO DO THIS ONCE]
 run_function("..\_Data", "*.edf", edf_to_asc) 
@@ -278,3 +390,8 @@ run_function("..\_Data", "*.asc", process_asc)
 
 # create new files with organized relevant data from existing .vmrk files
 run_function("..\_EEG", "*.vmrk", process_vmrk)
+
+# create single file with relevant data from all asc-derived EM files 
+run_Funtion("..\_Data", "*asc", process_EM_files)
+
+# get relevant tuples then create big list 
